68fea84d260aa6a231d0c7d731ef94f178256f6c
Elis Wagner Tornqvist <elis.wagnertornkvist@elev.ga.ntig.se>
2025-03-13 11:06:50 +0100

    tränat js, variabler och konkatenering

---
+++ kapitel-8/into/script.js
@@ -0,0 +1,14 @@
+// var ålder = 76;
+// var AnnanÅlder = 245;
+// var summa = ålder + AnnanÅlder;
+// var total = ålder * summa;
+// alert(ålder + " * " + summa + " = " + total);
+
+var fornamn = "Elis";
+var efternamn = "Wagner";
+var fullNamn = fornamn + " " + efternamn;
+var hobby = "Bouldering";
+var kostnad = 5000;
+var mening = hobby + " " + "är" + " " + fullNamn + "s" + " " + "hobby" + "." + " " + "\nHans bouldering hobby kostar" + " " + kostnad + "kr/år";
+
+alert(mening);
