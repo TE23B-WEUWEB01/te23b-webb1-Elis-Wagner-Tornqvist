85ac7d07dc6e19bd2b39c0f72be2f953962269cf
Elis Wagner Tornqvist <elis.wagnertornkvist@elev.ga.ntig.se>
2025-03-27 11:08:14 +0100

    tränat: onlcik, functioner, quarySelector

---
+++ kapitel-8/interaktiv/script.js
@@ -0,0 +1,37 @@
+function SkrivaText() {
+    document.body.style = "background: crimson; color: darkblue; text-align: center;";
+
+    document.querySelector("h1").style = "font-size: 50px;"
+
+     document.querySelector("p").style = "visibility: hidden;"
+}
+
+function Original() {
+    document.body.style = "background: white; text-color: black; text-align: center;";
+
+     document.querySelector("h1").style = "font-size: 50px;"
+
+      document.querySelector("p").style = "visibility: hidden;"
+
+}
+
+
+function KonstigRubrik() {
+    
+document.querySelector("h1").style = "font-size: 180px; text-decoration: overline; color: purple; text-shadow: 5px 5px 5px green;  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; "
+
+ document.querySelector("p").style = "visibility: hidden;"
+
+}
+
+
+function KlickaInte() {
+    
+    document.querySelector("p").style = "visibility: visible;"
+
+
+}
+
+
+
+
